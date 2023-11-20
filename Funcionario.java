53dd90bfa8643bf3ff795bd3d5517475eaf4a2e3
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-11-20 17:09:28 -0300

    Att. 20/11

--- Funcionario.java
+++ Funcionario.java
@@ -1,18 +1,33 @@
+import java.util.Date;
+
 public abstract class Funcionario {
     protected String nome;
     protected String endereco;
     protected String estadoCivil;
     protected int nroCarteiraTrabalho;
-    //cpf
-    //rg
-    //dataAdmissao
+    protected String cpf;
+    protected String rg;
+    protected Date dataAdmissao;
 
-    public Funcionario(String nome, String endereco, String estadoCivil, int nroCarteiraTrabalho){
+    public Funcionario(){
+        this.endereco = null;
+        this.estadoCivil = null;
+        this.nome = null;
+        this.nroCarteiraTrabalho = 0;
+        this.cpf = null;
+        this.rg = null;
+        this.dataAdmissao = new Date(0, 0, 0);
+    }
+
+    public Funcionario(String nome, String endereco, String estadoCivil, int nroCarteiraTrabalho, String cpf, String rg, int day, int month, int year){
         this.endereco = endereco;
         this.estadoCivil = estadoCivil;
         this.nome = nome;
         this.nroCarteiraTrabalho = nroCarteiraTrabalho;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.dataAdmissao = new Date(year, month, day);
     }
 
-    //public abstract float calcularSalario(); 
+    public abstract float calcularSalario();
 }
