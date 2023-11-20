a6073b769c54c2bbd5a9f86e6ac887a78e713583
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-11-20 17:09:56 -0300

    Att. 20/11

--- Garcom.java
+++ Garcom.java
@@ -1,10 +1,23 @@
 public class Garcom extends Funcionario{
     private float salarioBase;
     private String diaFolga;
+    private static float limite = 100;
+    private static float grat = 500;
 
-    public Garcom(String nome, String endereco, String estadoCivil, int nroCarteiraTrabalho, float salarioBase, String diaFolga){
-        super(nome, endereco, estadoCivil, nroCarteiraTrabalho);
+    public Garcom(String nome, String endereco, String estadoCivil, int nroCarteiraTrabalho, String cpf, String rg, int day, int month, int year, float salarioBase, String diaFolga){
+        super(nome, endereco, estadoCivil, nroCarteiraTrabalho, cpf, rg, day, month, year);
         this.salarioBase = salarioBase;
         this.diaFolga = diaFolga;
+    }
+
+    @Override
+    public float calcularSalario(){
+        if(/*pedidos*/ >= limite){
+            return salarioBase + grat;
+        }
+        else{
+            return salarioBase;
+
+        }
     }
 }
