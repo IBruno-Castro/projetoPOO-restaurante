638708f8c007323f3e2ce513f22f73420c88c847
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-11-20 17:07:51 -0300

    Att. 20/11

--- Cozinheiro.java
+++ Cozinheiro.java
@@ -1,7 +1,15 @@
 public class Cozinheiro extends Funcionario{
     //private listaPratos
 
-    public Cozinheiro(String nome, String endereco, String estadoCivil, int nroCarteiraTrabalho){
-        super(nome, endereco, estadoCivil, nroCarteiraTrabalho);
+    private static float pratoP = 30;
+    private static float sobremesa = 20;
+
+    public Cozinheiro(String nome, String endereco, String estadoCivil, int nroCarteiraTrabalho, String cpf, String rg, int day, int month, int year){
+        super(nome, endereco, estadoCivil, nroCarteiraTrabalho, cpf, rg, day, month, year);
+    }
+
+    @Override
+    public float calcularSalario() {
+        return 0;
     }
 }
