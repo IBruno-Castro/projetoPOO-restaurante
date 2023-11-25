85dc5136623d4511a5c99534ec1fedf685bfb06e
Jean Souto <jeansouto123@gmail.com>
2023-11-25 16:11:48 -0300

    lalala

--- PratoPrincipal.java
+++
@@ -1,5 +0,0 @@
-public class PratoPrincipal extends Prato{
-    public PratoPrincipal(String nomeComercial, float precoUni, float precoCusto, String descricao, int tempoPreparo){
-        super(nomeComercial, precoUni, precoCusto, descricao, tempoPreparo);
-    }
-}
--- Sobremesa.java
+++
@@ -1,8 +0,0 @@
-public class Sobremesa extends Prato{
-    private float nroCal;
-
-    public Sobremesa(String nomeComercial, float precoUni, float precoCusto, String descricao, int tempoPreparo, float nroCal){
-        super(nomeComercial, precoUni, precoCusto, descricao, tempoPreparo);
-        this.nroCal = nroCal;
-    }
-}
