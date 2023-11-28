f098aaac5de454e8a108865aa61679c91e8b9846
Jean Souto <jeansouto123@gmail.com>
2023-11-28 17:31:01 -0300

    28/11 - 17h30 - Incluindo pasta de músicas

--- Códigos/src/SoundTrack.java
+++ Códigos/src/SoundTrack.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class SoundTrack {
 
-    public void Musica1 () {
+    public void MusicFundo () {
         try {
             //local path do arquivo .wav de audio
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\jeans\\OneDrive\\IntelliJ\\POO1\\testes\\lalala.wav.wav").getAbsoluteFile());
@@ -25,7 +25,32 @@ public class SoundTrack {
             //genérico, pois há diversos erros possíveis
         } catch (Exception ex) {
             System.out.println("Erro ao executar SOM!");
-            ex.printStackTrace();
+            //ex.printStackTrace();
+        }
+    }
+
+    public void MusicPedidoConcluido () {
+        try {
+            //local path do arquivo .wav de audio
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\jeans\\OneDrive\\IntelliJ\\POO1\\testes\\lalala.wav.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY); //Para repetir o som até o fim da execução. 0 para repetir uma só vez
+            // clip.close(); fecha o som
+            // clip.stop(); pausa a música
+
+            // só de teste:
+            /*SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    JOptionPane.showMessageDialog(null, "Clique pra fechar!");
+                }
+            });*/
+
+            //genérico, pois há diversos erros possíveis
+        } catch (Exception ex) {
+            System.out.println("Erro ao executar SOM!");
+            //ex.printStackTrace();
         }
     }
 }
